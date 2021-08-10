5c521245eafeb925264900ba9bcde0503c8bc171
souadmaniani <soadmaniani98@gmail.com>
2021-08-10 18:48:29 +0000

    iterator_traits

--- iterator.md
+++ iterator.md
@@ -1,1 +1,21 @@
 # iterator:
+#### template<
+    class Category,
+    class T,
+    class Distance = std::ptrdiff_t,
+    class Pointer = T*,
+    class Reference = T&
+#### > struct iterator;
+### Category 
+- Input Iterator (Read only, forward moving)
+- Output Iterator (Write only, forward moving)
+- Forward Iterator (Read and Write, forward moving)
+- Biderctional Iterator (Read and Write, moves forward or backward)
+- Random Access Iterator (Read and Write, random access)
+[link] (https://www.codeproject.com/Articles/36530/An-Introduction-to-Iterator-Traits)
+# iterator_traits
+#### template <class Iterator> class iterator_traits;
+#### template <class T> class iterator_traits<T*>;
+#### template <class T> class iterator_traits<const T*>;
+- Traits class defining properties of iterators.
+
--- test.cpp
+++ test.cpp
@@ -1,19 +1,66 @@
+
+#include <vector>
+#include <deque>
+#include <forward_list>
+#include <list>
+#include <stack>
+#include <queue>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
 #include <iostream>
-#include <memory>
+#include <type_traits>
+#include <typeinfo>
 #include <string>
-#include <stdlib.h>
-#include<string.h>
-#include "test.hpp"
 using namespace std;
 
-// template< class T >
-// struct allocator;
-template<>
-struct allocator<void>;
-int main()
-{
-       // default allocator for ints
-        std::allocator<int> alloc;
-        int* p = alloc.allocate(1);
-        alloc.deallocate(p, 1); 
+// template<>
+// struct allocator<void>;
+//        // default allocator for ints
+//         std::allocator<int> alloc;
+//         int* p = alloc.allocate(1);
+//         alloc.deallocate(p, 1);
+
+
+template<typename T>
+std::string get_iterator_type (T it) {
+        // __normal_iterator<int *, vector<int, allocator<int> > > >
+        if      (typeid (std::iterator_traits<__normal_iterator<int *, vector<int, allocator<int> > > >::iterator_category) == typeid (std::input_iterator_tag))	  return "Input";
+        // else if (typeid (std::iterator_traits<T>::iterator_category) == typeid (std::output_iterator_tag))        return "Output";
+        // else if (typeid (std::iterator_traits<T>::iterator_category) == typeid (std::forward_iterator_tag))       return "Forward";
+        // else if (typeid (std::iterator_traits<T>::iterator_category) == typeid (std::bidirectional_iterator_tag)) return "Bidirectional";
+        // else if (typeid (std::iterator_traits<T>::iterator_category) == typeid (std::random_access_iterator_tag)) return "Random Access";
+
+        return "missing";
+}
+
+int main () {
+
+	std::vector<int> v;
+	// std::deque<int> d;
+	// std::forward_list<int> fl;
+	// std::list<int> l;
+
+	// std::set<int> s;
+	// std::map<int, int> m;
+	
+
+	std::vector<int>::iterator it1 = v.begin  ();
+	// std::deque<int>::iterator it2 = d.begin  ();
+	// std::forward_list<int>::iterator it3 = fl.begin ();
+	// std::list<int>::iterator it4 = l.begin  ();
+
+	// std::set<int>::iterator it5 = s.begin  ();
+	// std::map<int, int>::iterator it6 = m.begin  ();
+
+	std::cout << "\nSequence Containers:\n\n";
+	std::cout << "vector::begin        () returns a " << get_iterator_type (it1) << " Iterator.\n";
+	// std::cout << "deque::begin         () returns a " << get_iterator_type (it2) << " Iterator.\n";
+	// std::cout << "forward_list::begin  () returns a " << get_iterator_type (it3) << " Iterator.\n";
+
+	// std::cout << "\nAssociative Containers:\n\n";
+	// std::cout << "set::begin           () returns a " << get_iterator_type (it5) << " Iterator.\n";
+	// std::cout << "map::begin           () returns a " << get_iterator_type (it6) << " Iterator.\n";
+	return 0;
 }
