c415df9437738c6abbee076b5220cc56f815f5ec
souadmaniani <soadmaniani98@gmail.com>
2021-08-12 22:13:27 +0000

    SFINAE

--- iterator.md
+++ iterator.md
@@ -18,4 +18,10 @@
     template <class T> class iterator_traits<const T*>;
 - Traits class defining properties of iterators.
 - [iterator_traits] (https://www.codeproject.com/Articles/36530/An-Introduction-to-Iterator-Traits)
+# reverse_iterator
+    template< class Iter > class reverse_iterator;
+# enable_if
+    template <bool Cond, class T = void> struct enable_if;
+- SFINAE: Substitution Failure is not an Error, means that if the compiler fails to specialize a template with a given template argument list, it does not issue an error. It does when it cannot find any suitable function/class to be called/instantiated (either due to a lack of suitably declared functions/classes or an error during instantiation).
+- [SFINAE] (https://www.thewolfsound.com/sfinae-substitution-failure-is-not-an-error/)
 
--- test.cpp
+++ test.cpp
@@ -1,66 +1,19 @@
-
-#include <vector>
-#include <deque>
-#include <forward_list>
-#include <list>
-#include <stack>
-#include <queue>
-#include <set>
-#include <map>
-#include <unordered_set>
-#include <unordered_map>
+// enable_if example: two ways of using enable_if
 #include <iostream>
 #include <type_traits>
-#include <typeinfo>
-#include <string>
-using namespace std;
 
-// template<>
-// struct allocator<void>;
-//        // default allocator for ints
-//         std::allocator<int> alloc;
-//         int* p = alloc.allocate(1);
-//         alloc.deallocate(p, 1);
+// 1. the return type (bool) is only valid if T is an integral type:
+template <class T>
+typename std::enable_if< std::is_integral<T>::value, bool >::type  is_odd (T i) {
+	return bool(i%2);
+	}
 
+int main() {
 
-template<typename T>
-std::string get_iterator_type (T it) {
-        // __normal_iterator<int *, vector<int, allocator<int> > > >
-        if      (typeid (std::iterator_traits<__normal_iterator<int *, vector<int, allocator<int> > > >::iterator_category) == typeid (std::input_iterator_tag))	  return "Input";
-        // else if (typeid (std::iterator_traits<T>::iterator_category) == typeid (std::output_iterator_tag))        return "Output";
-        // else if (typeid (std::iterator_traits<T>::iterator_category) == typeid (std::forward_iterator_tag))       return "Forward";
-        // else if (typeid (std::iterator_traits<T>::iterator_category) == typeid (std::bidirectional_iterator_tag)) return "Bidirectional";
-        // else if (typeid (std::iterator_traits<T>::iterator_category) == typeid (std::random_access_iterator_tag)) return "Random Access";
+  int * i ;    // code does not compile if type of i is not integral
 
-        return "missing";
-}
+//   std::cout << std::boolalpha;
+  std::cout << "i is odd: " << is_odd(i) << std::endl;
 
-int main () {
-
-	std::vector<int> v;
-	// std::deque<int> d;
-	// std::forward_list<int> fl;
-	// std::list<int> l;
-
-	// std::set<int> s;
-	// std::map<int, int> m;
-	
-
-	std::vector<int>::iterator it1 = v.begin  ();
-	// std::deque<int>::iterator it2 = d.begin  ();
-	// std::forward_list<int>::iterator it3 = fl.begin ();
-	// std::list<int>::iterator it4 = l.begin  ();
-
-	// std::set<int>::iterator it5 = s.begin  ();
-	// std::map<int, int>::iterator it6 = m.begin  ();
-
-	std::cout << "\nSequence Containers:\n\n";
-	std::cout << "vector::begin        () returns a " << get_iterator_type (it1) << " Iterator.\n";
-	// std::cout << "deque::begin         () returns a " << get_iterator_type (it2) << " Iterator.\n";
-	// std::cout << "forward_list::begin  () returns a " << get_iterator_type (it3) << " Iterator.\n";
-
-	// std::cout << "\nAssociative Containers:\n\n";
-	// std::cout << "set::begin           () returns a " << get_iterator_type (it5) << " Iterator.\n";
-	// std::cout << "map::begin           () returns a " << get_iterator_type (it6) << " Iterator.\n";
-	return 0;
+  return 0;
 }
